8409763139a36926debea43fcc3a64745fee30ba
herayang <47582117+herayang@users.noreply.github.com>
2020-04-07 17:48:41 -0600

    Temporary UI scene
    This was the scene I was working on

---
+++ VirtualBox3DHomeTour/Assets/Scripts/ChangeButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+	public GameObject menu;
+	private bool visible = false;
+	public static ConsoleButton ins; 
+	private void OnTriggerEnter(Collider other)
+	{
+		if (visible)
+		{
+			menu.SetActive(true);
+			visible = true;
+		}
+		else {
+			menu.SetActive(false);
+			visible = false; 
+		}
+
+	}
+
+}
